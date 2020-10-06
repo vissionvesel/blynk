2a5e66163560692f7fe32b1b272e28d8c93bc30d
vissionvesel <heartengineer1999@gmail.com>
2020-10-06 20:31:57 +0530

    pin intitialization

---
+++ arduino/arduino.ino
@@ -0,0 +1,16 @@
+int dtime=1000;
+int pin=8;
+
+void setup() {
+  pinMode(pin,OUTPUT);
+  
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   digitalWrite(OUTPUT,HIGH);
+  // put your main code here, to run repeatedly:
+
+}
